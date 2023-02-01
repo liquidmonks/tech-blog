8eaa106f1e8f6d8f645b187e50ff357b0a1e64a2
Liquid Monks <info@liquidmonks.com>
2023-01-31 21:23:38 -0600

    package.json includes additional package dependencies.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -6,7 +6,7 @@ const userRoutes = require("./user-routes");
 const postRoutes = require("./post-routes");
 
 // Create all our routes and set up logic within those routes where required.
-router.use("/users", userRoutes);
+//router.use("/users", userRoutes);
 router.use("/post", postRoutes);
 
 // Export routes for server.js to use.
--- server.js
+++ server.js
@@ -1,20 +1,41 @@
-// Import the functions you need from the SDKs you need
 const path = require("path");
 const express = require("express");
-
-// TODO: Replace with your app's Firebase project configuration
+const session = require("express-session");
+const exphbs = require("express-handlebars");
 const routes = require("./controllers");
+const helpers = require("./utils/helpers");
+
 const sequelize = require("./config/connection");
-//const helpers = require("./utils/helpers");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-// Initialize Express
-const app = express();
-const PORT = process.env.PORT || 3001;
+const app = express(); // assign express function to 'app'
+const PORT = process.env.PORT || 3003; // set up port to be heroku or 3001.
 
-// Define middleware here
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+// Inform Express.js on which template engine to use
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use(routes);
 
-// Syncing our database and logging a message to the user upon success
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Server listening on Port: ", PORT));
-});
+sequelize.sync();
+
+app.listen(PORT, () => console.log(`Now listening on port http://localhost:${PORT}`));
