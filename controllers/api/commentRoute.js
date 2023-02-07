226ef7425ab69772bae8af3be0f858f257803c6d
Liquid Monks <info@liquidmonks.com>
2023-02-07 11:05:19 -0600

    commentRoute.js includes additional dev comments.

--- controllers/api/commentRoute.js
+++ controllers/api/commentRoute.js
@@ -1,19 +1,19 @@
-const router = require('express').Router();
-const { Comment } = require('../../models');
+const router = require("express").Router();
+const { Comment } = require("../../models");
 
-router.post('/:id', async (req, res) => {
-    try {
-        const message = await Comment.create({
-            ...req.body,
-            post_id: req.params.id,
-            user_id: req.session.user_id
-        });
-        res.json({message})
-    } catch (err) {
-        res.status(500).json(err);
-    }
-})
+router.post("/:id", async (req, res) => {
+  // add comment to post
+  try {
+    const message = await Comment.create({
+      //
+      ...req.body,
+      post_id: req.params.id,
+      user_id: req.session.user_id,
+    });
+    res.json({ message });
+  } catch (err) {
+    res.status(500).json(err); // if there is an error, send it back to the client
+  }
+});
 
-
-
-module.exports = router;
+module.exports = router; // exporting the router
