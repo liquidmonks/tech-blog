e1444ecf2b81e064f3471e4a91d86677be727e6d
Liquid Monks <info@liquidmonks.com>
2023-02-07 10:38:07 -0600

    Post.js includes additional dev comments.

--- models/Post.js
+++ models/Post.js
@@ -1,51 +1,55 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
+// create our Post model
 class Post extends Model {}
 
-Post.init (
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-          },
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1, 50],
-            },
-        },
-        contents: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-            validate: {
-                len: [1, 250]
-            }
-        },
-       
-        user_id: {
-            type: DataTypes.INTEGER,
-            allowNull: true,
-            references: {
-                model: 'user',
-                key:'id'
-            },
-        },  
+// create fields/columns for Post model
+Post.init(
+  {
+    id: {
+      // define the id column
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      // define the title column
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 50],
+      },
+    },
+    contents: {
+      // define the contents column
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [1, 250],
+      },
     },
 
-    {
-        sequelize,
-        timestamps: true,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'post',
+    user_id: {
+      //
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: "user",
+        key: "id",
+      },
     },
+  },
+
+  {
+    // pass in our imported sequelize connection (the direct connection to our database)
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "post",
+  }
 );
 
-
-
-
-module.exports = Post;
+module.exports = Post; // exporting the model
